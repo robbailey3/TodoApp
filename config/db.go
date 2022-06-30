395d4e154100dafd16571130e54cb04c2a6df2d9
Rob Bailey <rob.bailey3@gmail.com>
2022-06-30 08:54:58 +0100

    Trying with sslmode

--- config/db.go
+++ config/db.go
@@ -12,7 +12,7 @@ import (
 var DbConn *gorm.DB
 
 func InitDatabase() error {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/London", os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=Europe/London", os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
