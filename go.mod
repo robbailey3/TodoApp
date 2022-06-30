271692696e654aaa307557adce5203e350c6f19b
Rob Bailey <rob.bailey3@gmail.com>
2022-06-30 19:50:24 +0100

    Fixing dep issue and updating base url

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/robbailey3/todo-app
 go 1.18
 
 require (
+	github.com/go-playground/validator/v10 v10.11.0
 	github.com/gofiber/fiber/v2 v2.34.1
+	github.com/gofiber/helmet/v2 v2.2.13
 	github.com/joho/godotenv v1.4.0
 	gorm.io/driver/postgres v1.3.7
 	gorm.io/gorm v1.23.6
@@ -13,8 +15,6 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/gofiber/helmet/v2 v2.2.13 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -25,7 +25,7 @@ require (
 	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.15.7 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
@@ -33,5 +33,4 @@ require (
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- ui/src/services/tasksService.ts
+++ ui/src/services/tasksService.ts
@@ -3,31 +3,33 @@ import type { Task } from '@/models/task';
 import axios from 'axios';
 
 class TasksService {
-  private readonly http = axios.create({
-    baseURL: '/api/tasks'
-  });
+	private readonly http = axios.create({
+		baseURL: import.meta.env.PROD
+			? '/api/tasks'
+			: 'http://localhost:8000/api/tasks'
+	});
 
-  public async getTasks(): Promise<Task[]> {
-    const response = await this.http.get<ApiResponse<Task[]>>('');
+	public async getTasks(): Promise<Task[]> {
+		const response = await this.http.get<ApiResponse<Task[]>>('');
 
-    return response.data.result;
-  }
+		return response.data.result;
+	}
 
-  public async createTask(title: string): Promise<Task> {
-    const response = await this.http.post<ApiResponse<Task>>('', {
-      title
-    });
+	public async createTask(title: string): Promise<Task> {
+		const response = await this.http.post<ApiResponse<Task>>('', {
+			title
+		});
 
-    return response.data.result;
-  }
+		return response.data.result;
+	}
 
-  public async updateTask(task: Task): Promise<void> {
-    await this.http.patch(`/${task.id}`, { ...task });
-  }
+	public async updateTask(task: Task): Promise<void> {
+		await this.http.patch(`/${task.id}`, { ...task });
+	}
 
-  public async deleteTask(taskId: number): Promise<void> {
-    await this.http.delete(`/${taskId}`);
-  }
+	public async deleteTask(taskId: number): Promise<void> {
+		await this.http.delete(`/${taskId}`);
+	}
 }
 
 export default new TasksService();
